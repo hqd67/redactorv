525c8206cc68341803312b05936d4f16398f5d00
артем <artemlemur704@gmail.com>
2025-10-26 05:54:13 +0300

    добавлен базовый абстрактный класс для всех фигур.

---
+++ redactorv/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace redactorv
+{
+    public abstract class Shape
+    {
+        public Color Color { get; set; }
+        public bool IsSelected { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+            IsSelected = false;
+        }
+
+        public abstract void Draw(Graphics g);
+        public abstract bool ContainsPoint(Point p);
+        public abstract void Move(int dx, int dy);
+    }
+}

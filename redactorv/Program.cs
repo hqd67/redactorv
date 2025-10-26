786eebd8d1877b26e2f8a645359c7eb6af451fa0
артем <artemlemur704@gmail.com>
2025-10-26 06:34:13 +0300

    исправил ошибки

--- redactorv/Program.cs
+++ redactorv/Program.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using redactorv;
+using System;
 using System.Windows.Forms;
 
-namespace redactorv
+namespace redactor
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
